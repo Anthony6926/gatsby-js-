30f5716e1a0f71295a23c615776369445b40e707
Anthony sayegh <Sayeghanthony89012@gmail.com>
2022-02-20 21:18:55 +0200

    creating components

---
+++ gatsby-starter-hello-world/src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout';
+import Navbar from "../components/Navbar";
+
+const about = () => {
+  return (
+    <Layout>
+        
+      hello from about
+
+    </Layout>
+  )
+}
+
+export default about
--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -1,5 +1,16 @@
-import React from "react"
-
+import React from "react";
+import {Link} from 'gatsby';
+import Layout from '../components/Layout'
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+     
+      <h1>
+        
+        Hello people
+        
+      </h1>
+      
+    </Layout>
+  )
 }
